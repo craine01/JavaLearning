9e658c2985af426ddcad96a45882ad34861a103e
Craiii24 <kentreyes208@gmail.com>
2026-09-15 09:29:09 +0800

    Add files via upload

---
+++ boolean (5).java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class main {
+  public static void main (String [] args) {
+  int  x = 20;
+  int y = 10;
+  boolean checkNum = x == y;
+  /* true */
+  boolean checkNo = x != y;
+  /* false */
+  boolean checkData = x > y;
+  /* false */
+    System.out.println(checkData);
+  }
+}
